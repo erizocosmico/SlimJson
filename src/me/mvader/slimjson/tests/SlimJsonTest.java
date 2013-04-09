2db47d57638358d35bbeb5a3b2d974b397bd5cdf
mvader <rd4091@gmail.com>
2013-04-09 11:07:20 +0200

    Fix...

--- src/me/mvader/slimjson/tests/SlimJsonTest.java
+++ src/me/mvader/slimjson/tests/SlimJsonTest.java
@@ -1,9 +1,10 @@
 package me.mvader.slimjson.tests;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
 import java.util.Map;
-import me.mvader.slimjson.*;
-import org.junit.Test;
+import me.mvader.slimjson.SlimJson;
+import me.mvader.slimjson.JsonValue;
 
 public class SlimJsonTest {
 
